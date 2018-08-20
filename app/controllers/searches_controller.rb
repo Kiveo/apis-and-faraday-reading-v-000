abb5aed83869abc7718d8dc603c8729bfa12f2a7
Maverick Tesini <mavericktesini@gmail.com>
2018-08-20 04:23:17 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,8 +2,8 @@ class SearchesController < ApplicationController
 
   def search
     @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-      req.params['client_id'] = KLDTZ0SWOTENW0EFWBNZ1E5UKLVKTOW4UQXQLM0IGMKIFP3I
-      req.params['client_secret'] = RKDSN1Z4U3GETD5NVOLW30IUFCPIVGBAOA1IUA311KLBYO1V
+      req.params['client_id'] = 'KLDTZ0SWOTENW0EFWBNZ1E5UKLVKTOW4UQXQLM0IGMKIFP3I'
+      req.params['client_secret'] = 'RKDSN1Z4U3GETD5NVOLW30IUFCPIVGBAOA1IUA311KLBYO1V'
       req.params['v'] = '20160201'
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
