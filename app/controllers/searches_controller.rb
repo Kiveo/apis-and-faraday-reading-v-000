55ad70df1dca01f1721e25998e5c0ba5637b37e9
Maverick Tesini <mavericktesini@gmail.com>
2018-08-20 04:26:50 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,7 +8,7 @@ class SearchesController < ApplicationController
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
     end
-    render 'search'
+    render '/search'
   end
 
   def foursquare
